646b3096cde3720ca9d6c99463448d792984edab
dinata78 <stevendinata78@gmail.com>
2024-11-23 22:07:10 +0700

    Add each DOM element selectors

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+
+const acButton = document.querySelector("#ac-button");
+const percentageButton = document.querySelector("#percentage-button");
+const deleteButton = document.querySelector("#delete-button");
+const addButton = document.querySelector("#add-button");
+const subtractButton = document.querySelector("#subtract-button");
+const multiplyButton = document.querySelector("#multiply-button");
+const divideButton = document.querySelector("#divide-button");
+const dotButton = document.querySelector("#dot-button");
+const resultButton = document.querySelector("#result-button");
+
+const oneButton = document.querySelector("#one-button");
+const twoButton = document.querySelector("#two-button");
+const threeButton = document.querySelector("#three-button");
+const fourButton = document.querySelector("#four-button");
+const fiveButton = document.querySelector("#five-button");
+const sixButton = document.querySelector("#six-button");
+const sevenButton = document.querySelector("#seven-button");
+const eightButton = document.querySelector("#eight-button");
+const nineButton = document.querySelector("#nine-button");
+const zeroButton = document.querySelector("#zero-button");
+const doubleZeroButton = document.querySelector("#double-zero-button");
+
+const calculatorDisplay = document.querySelector("#calculator-display");
